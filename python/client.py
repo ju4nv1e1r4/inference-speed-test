902123084694e1cadc49b90c5f64f01b5786d51b
Juan Vieira <161749246+ju4nv1e1r4@users.noreply.github.com>
2025-02-25 11:21:58 -0300

    docs: add comments

--- python/client.py
+++ python/client.py
@@ -3,15 +3,17 @@ import churn_pb2
 import churn_pb2_grpc
 
 def run():
-    channel = grpc.insecure_channel("localhost:50051")
+    channel = grpc.insecure_channel("localhost:50051") # Seremos cliente nessa porta
     stub = churn_pb2_grpc.ChurnServiceStub(channel)
-
+    
+    # Criamos o request com as features
     request = churn_pb2.Features(
         gender="Male",
         subscription_type="Premium",
         contract_length="Annual"
     )
-
+    
+    # Imprimimos a resposta na tela
     response = stub.Predict(request)
     print(f"Predição de Churn: {response.churn}")
 
