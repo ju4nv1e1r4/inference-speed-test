5081888deac62910bbe3927d3690aa6c6e2703fd
Juan Vieira <161749246+ju4nv1e1r4@users.noreply.github.com>
2025-02-25 01:00:37 -0300

    style: irrelevant stuffs are changed

--- python/client.py
+++ python/client.py
@@ -3,18 +3,15 @@ import churn_pb2
 import churn_pb2_grpc
 
 def run():
-    # Criar um canal gRPC
     channel = grpc.insecure_channel("localhost:50051")
     stub = churn_pb2_grpc.ChurnServiceStub(channel)
 
-    # Criar dados de entrada
     request = churn_pb2.Features(
         gender="Male",
         subscription_type="Premium",
         contract_length="Annual"
     )
 
-    # Chamar o serviço
     response = stub.Predict(request)
     print(f"Predição de Churn: {response.churn}")
 
