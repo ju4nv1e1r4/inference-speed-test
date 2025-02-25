d87b105cbada72c611b548900e1bfe186344b8c6
Juan Vieira <161749246+ju4nv1e1r4@users.noreply.github.com>
2025-02-25 11:24:19 -0300

    docs: add comments

--- go/main.go
+++ go/main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"time"
+ "context"
+ "fmt"
+ "log"
+ "time"
 
-	pb "predict/churnpb" // package dos arquivos gerados pelo .proto
+ pb "predict/churnpb" // package dos arquivos gerados pelo .proto
 
-	"google.golang.org/grpc"
+ "google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure()) // Ajuste a porta conforme necessário
-	if err != nil {
-		log.Fatalf("Falha ao conectar ao servidor: %v", err)
-	}
-	defer conn.Close()
+ // Conecta com o servidor
+ conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+ if err != nil {
+  log.Fatalf("Falha ao conectar ao servidor: %v", err)
+ }
+ defer conn.Close()
+ 
+ // Cria o client
+ client := pb.NewChurnServiceClient(conn)
 
-	client := pb.NewChurnServiceClient(conn) // Ajuste para ChurnService
+ ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+ defer cancel()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+ // Cria uma requisição com os dados
+ req := &pb.Features{
+  Gender:           "Male",
+  SubscriptionType: "Premium",
+  ContractLength:   "Annual",
+ }
 
-	
-	req := &pb.Features{
-		Gender:           "Male",
-		SubscriptionType: "Premium",
-		ContractLength:   "Annual",
-	}
+ // Realiza a inferência
+ resp, err := client.Predict(ctx, req) //
+ if err != nil {
+  log.Fatalf("Erro ao chamar Predict: %v", err)
+ }
 
-	
-	resp, err := client.Predict(ctx, req) //
-	if err != nil {
-		log.Fatalf("Erro ao chamar Predict: %v", err)
-	}
-
-	// Realiza inferência
-	fmt.Printf("Churn previsto: %d\n", resp.Churn)
-}
+ // Imprime a resposta da inferência
+ fmt.Printf("Churn previsto: %d\n", resp.Churn)
